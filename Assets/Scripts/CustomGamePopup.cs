b46c3f5a09ce3940de7d350a0fff8fbd1787c0d0
James Goodman <james.goodman@maginteractive.se>
2024-01-17 17:41:04 +0000

    Added placeholder for custom game popup

---
+++ Assets/Scripts/CustomGamePopup.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GoodHub.Core.Runtime.PopupSystem;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomGamePopup : PopupBase
+{
+    [SerializeField] private TMP_InputField _seedInputField;
+    [SerializeField] private Slider _botDifficultySlider;
+    [SerializeField] private Slider _startingCoverageSlider;
+
+    private void Start()
+    {
+        _seedInputField.
+        
+        
+    }
+}
